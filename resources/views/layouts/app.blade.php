828cba524d6215e33f4a7a48e4a02ecb9a199499
Roger <roger.r.oliveira@unesp.br>
2023-09-18 21:49:13 -0300

    :lipstick: feat: Alterações app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,11 @@
         <!-- Styles -->
         @livewireStyles
     </head>
+	
     <body class="font-sans antialiased">
-        <x-banner />
-
+		
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @livewire('navigation-menu')
-
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
@@ -34,7 +33,9 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                <div class="container">
+					{{ $slot }}
+				</div>
             </main>
         </div>
 
